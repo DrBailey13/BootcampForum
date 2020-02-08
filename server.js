344c890977ae7394f1d14231219890c4c6efa304
Tcauley91 <caleytyler@gmail.com>
2020-02-08 11:31:45 -0500

    remove force sync

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/', routes);
 
 // Start our server so that it can begin listening to client requests.
 
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log('Server listening on: http://localhost:' + PORT);
